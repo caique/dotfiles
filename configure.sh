95fbe1b4175eebaa0f5333d3353f0cc9fbae1260
Caique Peixoto <caiquepeixoto1@gmail.com>
2020-03-14 15:54:36 +0100

    Improve configure.sh by cleaning useless profile files and installing SDKMAN.

--- configure.sh
+++ configure.sh
@@ -1,2 +1,13 @@
 # Configure zsh as default shell
-chsh -s $(which zsh)
+chsh -s $(command -v zsh)
+
+# Cleanup useless profile files and generate new .zshrc
+FILESTOCLEANUP=(.bash_profile .zprofile .zsh_history .zshrc .zshrc.zwc)
+for FILE in "${FILESTOCLEANUP[@]}"; do
+	rm ${HOME}/$FILE
+done
+
+echo "# Generated from .dotfiles repository\nsouroe ~/.dotfiles/.zshrc" > ${HOME}/.zshrc
+
+# Install SDKMan
+curl -s http://get.sdkman.io | bash
