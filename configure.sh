5f08b096226869e1fff964ffcedd824db45ffcb8
Caique Peixoto <caiquepeixoto1@gmail.com>
2020-03-16 15:37:17 +0100

    Minor adjustments on configure.sh.

--- configure.sh
+++ configure.sh
@@ -4,14 +4,13 @@ chsh -s $(command -v zsh)
 # Cleanup useless profile files and generate new .zshrc
 FILESTOCLEANUP=(.bash_profile .zprofile .zsh_history .zshrc .zshrc.zwc)
 for FILE in "${FILESTOCLEANUP[@]}"; do
-	rm ${HOME}/$FILE
+    rm ${HOME}/$FILE
 done
 
 echo "# Generated from .dotfiles repository\nsource ~/.dotfiles/.zshrc" > ${HOME}/.zshrc
 
 # Generate configurations for Neovim
-mkdir ${HOME}/.config
-mkdir ${HOME}/.config/nvim
+mkdir -p ${HOME}/.config/nvim
 echo "\" Generated from .dotfiles repository\nsource ~/.dotfiles/.vimrc" > ${HOME}/.config/nvim/init.vim
 
 # Install SDKMan
